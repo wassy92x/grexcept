0f7867582883bc15bd926bae5ae357abbda45280
Fabian Wassenhoven <fabian.wassenhoven@vomatec.de>
2021-11-13 15:36:37 +0100

    include README and LICENSE in npm package

---
+++ Code/bin/postPack.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+fs.rmSync('../README.md', './README.md');
+fs.rmSync('../LICENSE', './LICENSE');
---
+++ Code/bin/preparePack.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+fs.copyFileSync('../README.md', './README.md');
+fs.copyFileSync('../LICENSE', './LICENSE');
